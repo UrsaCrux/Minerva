0456274c1767f240cdddbb301ebeddba81aa210d
HechoEnChile_l <agusvideos34@gmail.com>
2026-01-28 22:16:51 -0300

    solucionado error de SSR

--- minerva-front/src/app/utils/perfil.js
+++ minerva-front/src/app/utils/perfil.js
@@ -2,10 +2,16 @@
 export default function Perfil() {
 
     function setToken(token, id){
+        if (typeof window === "undefined") {
+            return
+        }
         sessionStorage.setItem('pwd', token)
         sessionStorage.setItem('user_id', id)
     }
     function getToken(){
+        if (typeof window === "undefined") {
+            return { token: null, id_usuario: null }
+        }
         const token = sessionStorage.getItem('pwd')
         const id_usuario = sessionStorage.getItem('user_id')
         return {token: token, id_usuario: id_usuario}
